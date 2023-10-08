446aecde598544fcb3d26bd4f75e5c3c87a1f32f
Ahmed Mabrouk <dell@DESKTOP-5SJF271>
2023-10-08 18:05:28 +0300

    Adding viewModels class to work with it instead of models and adding some data annotation validation and auto mapper will added to map between model and view model

---
+++ EcommerceGame/ViewModels/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EcommerceGame.DAL.Models;
+
+namespace EcommerceGame.PL.ViewModels
+{
+    public class RoleViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<UserRole> userRoles { get; set; }
+    }
+}
---
+++ EcommerceGame/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceGame.DAL.Models;
+
+namespace EcommerceGame.PL.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public int User_Id { get; set; }
+        public User user { get; set; } // Navigation property
+        public int Role_ID { get; set; }
+        public Role role { get; set; } // Navigation property
+    }
+}
