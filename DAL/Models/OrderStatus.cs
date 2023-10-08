274e6c02efc473faad42f2e8a8b37e28848a2a23
Mohamed <Mohamed@DESKTOP-JGJ2QE6>
2023-10-08 22:42:01 +0300

    Add OrderStatus

---
+++ DAL/Models/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class OrderStatus
+{
+    [Table("OrderStatus")]
+    public OrderStatus()
+	{
+
+                public int Id { get; set; }
+    [Required]
+    public int StatusId { get; set; }
+    [Required]
+	[MaxLength(20)]
+	public string? statusname { get; set; }	
+
+}
+}
