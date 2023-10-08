2bf615e7471bcfe281a8c9abc1bc7b356b040cac
Ahmed Mabrouk <dell@DESKTOP-5SJF271>
2023-10-08 20:06:58 +0300

    just test

--- EcommerceGame/Controllers/HomeController.cs
+++ EcommerceGame/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace EcommerceGame.Controllers
-{
+{// test pull
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
