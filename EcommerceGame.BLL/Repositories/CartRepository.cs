7c3d429c63b94caff89c6ee66fd669dc5a69c60b
Sob7yy <mustafam7moud7@gmail.com>
2023-10-08 18:26:36 +0300

    Created a Cart Repository

---
+++ EcommerceGame.BLL/Repositories/CartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Demo.DAL.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceGame.BLL.Repositories
+{
+    internal class CartRepository
+    {
+        FinalMVCContext dbContext;
+
+        public CartRepository(FinalMVCContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
